5462dff8974f52dd2a886563ee449790277a17f3
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-10 23:36:24 -0300

    Pandas series, index

--- pandas-series/series.py
+++ pandas-series/series.py
@@ -1,4 +1,7 @@
 import pandas as pd
 names = ['Rodrigo', 'Fernando', 'Jéssica']
 data = pd.Series(names)
+data_index = pd.Series(names, ['nome 1', 'nome 2', 'nome 3'])
 print(data)
+print(data_index)
+print(data_index['nome 1'])
