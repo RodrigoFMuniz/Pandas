3fd43ba66b3ef457f7711da144497f1d6aa267a8
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-10 23:57:00 -0300

    Pandas dataframes, loc named index

--- pandas-dataframes/loc.py
+++ pandas-dataframes/loc.py
@@ -7,9 +7,12 @@ data = {
 
 #load data into a DataFrame object:
 df = pd.DataFrame(data)
+df2 = pd.DataFrame(data, index = ["day1", "day2", "day3"])
 
 print(df)
 print('-------------------')
 print(df.loc[0])
 print('-------------------')
 print(df.loc[[0, 1]])
+print('-------------------')
+print(df2)
